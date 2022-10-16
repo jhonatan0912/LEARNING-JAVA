71870e3fa07983e6de5af20b07d490cd7cfff379
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-15 19:34:52 -0500

    Hello world in JAVA

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World from java");
+
+        // When use a println the string is showed in complete line
+        System.out.println("This is a line with println");
+        System.out.println("This is another line  with println");
+
+
+    }
+}
