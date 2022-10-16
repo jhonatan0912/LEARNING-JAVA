f9d8ea87672a53acb8584d120a3de92a7c1d474e
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-15 19:35:26 -0500

    print in JAVA

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("This is a line with println");
         System.out.println("This is another line  with println");
 
-
+        // When we use a print the string is showed in line, can be multiple lines
+        System.out.print("This is a line just with print");
+        System.out.print("This is another line with print");
     }
 }
